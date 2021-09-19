36d9b236ff69b6e6a635a7f2b14cae9eeeeb5f7e
Ricardo Arana Reyes Guerrero <ricardo.aranareyes@gmail.com>
2021-09-18 21:16:51 -0500

    first vesion

--- apps/administration-e2e/src/integration/app.spec.ts
+++ apps/administration-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('my', () => {
+describe('administration', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('my', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to my!');
+    getGreeting().contains('Welcome to administration!');
   });
 });
--- libs/shared/jest.config.js
+++ libs/shared/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'my',
+  displayName: 'shared',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/my',
+  coverageDirectory: '../../coverage/libs/shared',
   transform: {
     '^.+\\.(ts|js|html)$': 'jest-preset-angular',
   },
